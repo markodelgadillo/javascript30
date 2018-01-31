50edac8f64af3ef9ef93aacf7e0c352b9d780213
marko delgadillo <markoneseven@gmail.com>
2018-01-30 21:11:05 -0800

    ##14 Used the some(), every() and find() array methods on the provided arrays. For more practice, converted the callback functions and code blocks to arrow functions and implicit returns.

--- array-cardio-2/index.js
+++ array-cardio-2/index.js
@@ -12,3 +12,33 @@ const comments = [
   { text: 'Rame is my fav food ever', id: 123523 },
   { text: 'Nice Nice Nice!', id: 542328 }
 ]
+
+// const isAdult = people.some(function(person) {
+//   const currentYear = new Date().getFullYear()
+//   if (currentYear - person.year >= 18) {
+//     return true
+//   }
+// })
+
+const isAdult = people.some(person => {
+  const currentYear = new Date().getFullYear()
+  return currentYear - person.year >= 18
+})
+console.log({ isAdult })
+
+const allAdults = people.every(person => {
+  new Date().getFullYear() - person.year >= 18
+})
+console.log({ allAdults })
+
+const comment = comments.find(comment => comment.id === 823423)
+console.log(comment)
+
+const index = comments.find(index => comment.id === 823423)
+console.log(index)
+
+// deletes the comment
+comments.splice(index, 1)
+
+const newComments = [...comments.slice(0, index), ...comments.slice(index + 1)]
+console.table(newComments)
