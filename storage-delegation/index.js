2b572bf0edbfd88f1787752fde10627912e8bdb7
marko delgadillo <markoneseven@gmail.com>
2018-03-10 21:46:32 -0800

    #33 A function to toggle the value of the "done" key in the data will now recognize if the ingredient is checked or unchecked.

--- storage-delegation/index.js
+++ storage-delegation/index.js
@@ -1,6 +1,11 @@
 const addItems = document.querySelector('.add-items')
+
 // itemsList is the unordered list in the html
 const itemsList = document.querySelector('.plates')
+
+// parses the data from localStorage back to JSON to make the
+// data available again to the functions again
+// OR will still function with an empty array
 const items = JSON.parse(localStorage.getItem('items')) || []
 
 function addItem(e) {
@@ -37,6 +42,22 @@ function populateList(plates = [], platesList) {
     .join('')
 }
 
-addItems.addEventListener('submit', addItem)
+function toggleDone(e) {
+  if (!e.target.matches('input')) {
+    return
+  }
+  const el = e.target
+  const index = el.dataset.index
+  console.log(index)
+  // sets the value of the done key in the index
+  // to the opposite of its current value
+  // i.e. If its true, it toggles to false
+  // and vice versa
+  items[index].done = !items[index].done
+}
 
+addItems.addEventListener('submit', addItem)
+itemsList.addEventListener('click', toggleDone)
+
+// runs the function on page load
 populateList(items, itemsList)
