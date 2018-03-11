a055d6eebd8e5e825b897330f9a1f5652b52d4d7
marko delgadillo <markoneseven@gmail.com>
2018-03-10 21:10:32 -0800

    #33 Functions to store the information and render list items completed.

---
+++ storage-delegation/index.js
@@ -0,0 +1,37 @@
+const addItems = document.querySelector('.add-items')
+// itemsList is the unordered list in the html
+const itemsList = document.querySelector('.plates')
+const items = []
+
+function addItem(e) {
+  e.preventDefault()
+  const text = this.querySelector('[name=item]').value
+  const item = {
+    text: text,
+    done: false
+  }
+  items.push(item)
+  populateList(items, itemsList)
+  this.reset()
+  console.table(items)
+}
+
+// items will be passed in as plates. It is an
+// empty array so that if nothing is passed in,
+// it will loop over instead of break the function
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, i) => {
+      return `
+    <li>
+      <input type="checkbox" data-index=${i} id="item${i}" ${
+        plate.done ? 'checked' : ''
+      }/>
+      <label for="item${i}">${plate.text}</label>
+    </li>
+  `
+    })
+    .join('')
+}
+
+addItems.addEventListener('submit', addItem)
