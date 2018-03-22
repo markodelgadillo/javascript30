0433a96a10fb6b43b7ea4de27a93dc732cb5991d
marko delgadillo <markoneseven@gmail.com>
2018-03-21 20:41:25 -0700

    #43 Added functions for an rgb effect as well as a red effect.

--- webcam-fun/index.js
+++ webcam-fun/index.js
@@ -25,6 +25,9 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height)
+    let pixels = ctx.getImageData(0, 0, width, height)
+    pixels = rgbSplit(pixels)
+    ctx.putImageData(pixels, 0, 0)
   }, 16)
 }
 
@@ -42,6 +45,24 @@ function takePhoto() {
   console.log(data)
 }
 
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0] // red
+    pixels.data[i + 100] = pixels.data[i + 1] // green
+    pixels.data[i - 150] = pixels.data[i + 2] // blue
+  }
+  return pixels
+}
+
 getVideo()
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100 // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50 // green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5 // blue
+  }
+  return pixels
+}
 
 video.addEventListener('canplay', paintToCanvas)
