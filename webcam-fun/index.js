db6b465a1231996d694099b7be918cd61851b5ad
marko delgadillo <markoneseven@gmail.com>
2018-03-22 21:40:03 -0700

    #43 Added range inputs to adjust rgb levels of the image painted onto the canvas.

--- webcam-fun/index.js
+++ webcam-fun/index.js
@@ -26,7 +26,9 @@ function paintToCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height)
     let pixels = ctx.getImageData(0, 0, width, height)
-    pixels = rgbSplit(pixels)
+    // pixels = rgbSplit(pixels)
+    pixels = greenScreen(pixels)
+    // ctx.globalAlpha = 0.5
     ctx.putImageData(pixels, 0, 0)
   }, 16)
 }
@@ -48,21 +50,48 @@ function takePhoto() {
 function rgbSplit(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
     pixels.data[i - 150] = pixels.data[i + 0] // red
-    pixels.data[i + 100] = pixels.data[i + 1] // green
+    pixels.data[i + 500] = pixels.data[i + 1] // green
     pixels.data[i - 150] = pixels.data[i + 2] // blue
   }
   return pixels
 }
 
-getVideo()
+function greenScreen(pixels) {
+  const levels = {}
 
-function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 100 // red
-    pixels.data[i + 1] = pixels.data[i + 1] - 50 // green
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5 // blue
+  document.querySelectorAll('.rgb input').forEach(input => {
+    levels[input.name] = input.value
+  })
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0]
+    green = pixels.data[i + 1]
+    blue = pixels.data[i + 2]
+    alpha = pixels.data[i + 3]
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      pixels.data[i + 3] = 0
+    }
   }
   return pixels
 }
+
+// function redEffect(pixels) {
+//   for (let i = 0; i < pixels.data.length; i += 4) {
+//     pixels.data[i + 0] = pixels.data[i + 0] + 100 // red
+//     pixels.data[i + 1] = pixels.data[i + 1] - 50 // green
+//     pixels.data[i + 2] = pixels.data[i + 2] * 0.5 // blue
+//   }
+//   return pixels
+// }
+
+getVideo()
 
 video.addEventListener('canplay', paintToCanvas)
