fe3ca562a23487dc3b8c6e30b92e3538b457aa67
marko delgadillo <markoneseven@gmail.com>
2018-03-27 22:58:40 -0700

    #45 Logic to access the browsers speech recognition functionality and to render the results onto the front end completed.

--- speech-rec/index.js
+++ speech-rec/index.js
@@ -5,9 +5,22 @@ const recognition = new SpeechRecognition()
 recognition.interimResults = true
 
 let p = document.createElement('p')
-const words = document.querySelecteor('.words')
+const words = document.querySelector('.words')
 words.appendChild(p)
 
-recognition.addEventListener('results', e => {
-  console.log(e)
+recognition.addEventListener('result', e => {
+  console.log(e.results)
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('')
+  console.log(transcript)
+  p.textContent = transcript
+  if (e.results[0].isFinal) {
+    p = document.createElement('p')
+    words.appendChild(p)
+  }
 })
+
+recognition.addEventListener('end', recognition.start)
+recognition.start()
