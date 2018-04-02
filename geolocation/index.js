e540e5cefb13812dfc8a1df2fc776b2d7094f240
marko delgadillo <markoneseven@gmail.com>
2018-04-01 22:35:59 -0700

    #47 Added logic to index.js to convert the speed data to miles per hour and changed the km/h in the html file to mph.

--- geolocation/index.js
+++ geolocation/index.js
@@ -4,7 +4,10 @@ const speed = document.querySelector('.speed-value')
 navigator.geolocation.watchPosition(
   data => {
     console.log(data)
-    speed.textContent = data.coords.speed
+    speed.textContent = Number.parseFloat(data.coords.speed * 2.23694).toFixed(
+      2
+    )
+    console.log(data.coords.speed)
     arrow.style.transform = `rotate(${data.coords.heading}deg)`
   },
   err => {
