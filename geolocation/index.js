802aa218511c0c21afb8016d839dee5cca79231d
marko delgadillo <markoneseven@gmail.com>
2018-04-01 22:15:49 -0700

    #47 Accessed the geolocation method for the navigator using the watchPosition method to continuosly get a speed from the data.

---
+++ geolocation/index.js
@@ -0,0 +1,7 @@
+const arrow = document.querySelector('.arrow')
+const speed = document.querySelector('.speed-value')
+
+navigator.geolocation.watchPosition(data => {
+  console.log(data)
+  speed.textContent = data.coords.speed
+})
