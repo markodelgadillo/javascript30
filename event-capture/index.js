8a54d2781ba832c9d9aa47885b70e9e8b11ecd8d
marko delgadillo <markoneseven@gmail.com>
2018-04-08 21:41:49 -0700

    #55 Created logic that shows examples of event capture, bubbling, propagation and once.

--- event-capture/index.js
+++ event-capture/index.js
@@ -1,7 +1,36 @@
 const divs = document.querySelectorAll('div')
+const button = document.querySelector('button')
 
 function logText(e) {
   console.log(this.classList.value)
+  /* When the Event Capture is set to false
+     this will stop on the event you originally clicked
+     and will not bubble up or capture down if Event Caputure is true */
+  e.stopPropagation()
 }
 
-divs.forEach(div => div.addEventListener('click', logText))
+divs.forEach(div =>
+  div.addEventListener('click', logText, {
+    /* Event capture by default is false
+       By setting it to true, it will trigger
+       the event on the capture going down
+       instead of the bubbling up
+       */
+    capture: false,
+    // listens for a click and unbinds itself
+    // meaning it then REMOVES the event listener
+    once: true
+  })
+)
+
+// This button will only trigger the function in
+// the event listener once!
+button.addEventListener(
+  'click',
+  () => {
+    console.log('Click!!!')
+  },
+  {
+    once: true
+  }
+)
