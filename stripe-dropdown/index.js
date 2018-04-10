2c97dff0375879496d3b4d29e7b33a4e0709dbed
marko delgadillo <markoneseven@gmail.com>
2018-04-09 22:28:19 -0700

    #57 Using css selectors, each function will add or remove a class list to each trigger list item that will change the opacity and display values.

--- stripe-dropdown/index.js
+++ stripe-dropdown/index.js
@@ -2,14 +2,16 @@ const triggers = document.querySelectorAll('.cool > li')
 const background = document.querySelector('.dropdownBackground')
 const nav = document.querySelector('.top')
 
-function handleEnter(){
+function handleEnter() {
   console.log('Enter!!!')
+  this.classList.add('trigger-enter')
+  setTimeout(() => this.classList.add('trigger-enter-active'), 150)
 }
 
-function handleLeave(){
+function handleLeave() {
   console.log('Leave!!!')
+  this.classList.remove('trigger-enter', 'trigger-enter-active')
 }
 
-triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter)
-triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave)
-))
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter))
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave))
