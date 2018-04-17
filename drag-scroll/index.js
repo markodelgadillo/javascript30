9b74200dfe3515c2a5805520803526e4619ba560
marko delgadillo <markoneseven@gmail.com>
2018-04-16 22:07:08 -0700

    #59 Added logic to grab and scroll the list items in regards to the starting position of the click and the location of the cursor when moving across the page.

--- drag-scroll/index.js
+++ drag-scroll/index.js
@@ -1,14 +1,32 @@
 const slider = document.querySelector('.items')
 let isDown = false
 let startX
-let ScrollLeft
+let scrollLeft
 
-slider.addEventListener('mousedown', () => {
+slider.addEventListener('mousedown', e => {
   isDown = true
+  slider.classList.add('active')
+  startX = e.pageX - slider.offsetLeft
+  scrollLeft = slider.scrollLeft
+  console.log(scrollLeft)
 })
 
-slider.addEventListener('mouseleave', () => {})
+slider.addEventListener('mouseleave', () => {
+  isDown = false
+  slider.classList.remove('active')
+})
 
-slider.addEventListener('mouseup', () => {})
+slider.addEventListener('mouseup', () => {
+  isDown = false
+  slider.classList.remove('active')
+})
 
-slider.addEventListener('mousemove', () => {})
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return
+  e.preventDefault()
+  const x = e.pageX - slider.offsetLeft
+  console.log({ x, startX })
+  console.log(isDown, 'Do Work!')
+  const walk = x - startX
+  slider.scrollLeft = scrollLeft - walk
+})
