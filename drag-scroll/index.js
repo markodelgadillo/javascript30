cc0cd2323a43137ccd8ca287398a99fc3c0c0d2d
marko delgadillo <markoneseven@gmail.com>
2018-04-16 21:35:14 -0700

    #59 Initial javascript logic to add event listeners to each item in the container div.

---
+++ drag-scroll/index.js
@@ -0,0 +1,14 @@
+const slider = document.querySelector('.items')
+let isDown = false
+let startX
+let ScrollLeft
+
+slider.addEventListener('mousedown', () => {
+  isDown = true
+})
+
+slider.addEventListener('mouseleave', () => {})
+
+slider.addEventListener('mouseup', () => {})
+
+slider.addEventListener('mousemove', () => {})
