c4edce8f8ba218f8e8be5d56f661bd3a0f3dec1a
marko delgadillo <markoneseven@gmail.com>
2018-04-17 22:35:16 -0700

    #61 Added logic to resize the style of the bar to move up and down like a slider.

--- video-ui/index.js
+++ video-ui/index.js
@@ -5,5 +5,10 @@ const video = document.querySelector('.flex')
 speed.addEventListener('mousemove', function(e) {
   const y = e.pageY - this.offsetTop
   const percent = Math.round(y / this.offsetHeight * 100) + '%'
+  //min and max are the multplier speeds
+  const min = 0.4
+  const max = 4
+  const height = percent
+  bar.style.height = height
   console.log(percent)
 })
