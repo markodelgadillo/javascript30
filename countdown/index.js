c5cbe59fa66b5b74ccee31d0c11abd916a5573ed
marko delgadillo <markoneseven@gmail.com>
2018-04-19 21:53:55 -0700

    #62 Timer now countdowns to zero.

--- countdown/index.js
+++ countdown/index.js
@@ -1,8 +1,19 @@
+let countDown
+
 function timer(seconds) {
   const now = Date.now()
   const then = now + seconds * 1000
-  setInterval(() => {
+  countDown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000)
-    console.log(secondsLeft)
+    //check if we should stop inspect
+    if (secondsLeft < 0) {
+      clearInterval(countDown)
+      return
+    }
+    displayTimeLeft(secondsLeft)
   }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+  console.log(seconds)
 }
