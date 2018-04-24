413230c81f38ee376db275aa1bce846ebfda5daa
marko delgadillo <markoneseven@gmail.com>
2018-04-23 20:36:35 -0700

    #62 Fixed the logic for the displayed time of when the counter would end.

--- countdown/index.js
+++ countdown/index.js
@@ -34,7 +34,9 @@ function displayEndTime(timestamp) {
   const end = new Date(timestamp)
   const hour = end.getHours()
   const minutes = end.getMinutes()
-  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes}`
+  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${
+    minutes < 10 ? '0' + minutes : minutes
+  }`
 }
 
 // function allZeroes(time) {
