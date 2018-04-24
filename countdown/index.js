d6ea28a54c633fdad01d195703e45cc124d6fe4f
marko delgadillo <markoneseven@gmail.com>
2018-04-23 20:28:56 -0700

    #62 Added a function to render a message that included the time the countdown would end.

--- countdown/index.js
+++ countdown/index.js
@@ -1,10 +1,12 @@
 let countDown
 const timeLeft = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
 
 function timer(seconds) {
   displayTimeLeft(seconds)
   const now = Date.now()
   const then = now + seconds * 1000
+  displayEndTime(then)
   countDown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000)
     //check if we should stop inspect
@@ -22,5 +24,23 @@ function displayTimeLeft(seconds) {
   if (remainderSeconds <= 9) {
     remainderSeconds = '0' + `${remainderSeconds}`
   }
-  timeLeft.textContent = `${minutes + ':' + remainderSeconds}`
+  const time = (timeLeft.textContent = `${minutes + ':' + remainderSeconds}`)
+  // adds timer to the browser tab
+  document.title = time
+  // allZeroes(time)
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp)
+  const hour = end.getHours()
+  const minutes = end.getMinutes()
+  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes}`
+}
+
+// function allZeroes(time) {
+//   if (time == '0:00'){
+//   setInterval(time => {
+//       time =
+//     }
+//   }, 250)
+// }
